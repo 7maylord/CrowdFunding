c4010fe4086d40cce2ce63f24638ffc1c097c0d9
Olumide Adenigba <olumideadenigba@gmail.com>
2025-02-13 19:56:51 +0100

    chore: test not test, vibes on script

---
+++ Smart-contract/scripts/deploy.ts
@@ -0,0 +1,65 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer, donor1, donor2, recipient] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.getAddress());
+
+  // Deploy the CTK Token
+  const CTK = await ethers.getContractFactory("CTK");
+  const ctk = await CTK.deploy();
+  await ctk.waitForDeployment();
+  console.log("CTK Token deployed at:", ctk.getAddress());
+
+  // Deploy the CrowdFunding Contract
+  const CrowdFunding = await ethers.getContractFactory("CrowdFunding");
+  const crowdfunding = await CrowdFunding.deploy(ctk.getAddress());
+  await crowdfunding.waitForDeployment();
+  console.log("CrowdFunding Contract deployed at:", crowdfunding.getAddress());
+
+  // Mint tokens to donors
+  const mintAmount = ethers.parseUnits("100", 18)
+  await ctk.mint(donor1.getAddress(), mintAmount);
+  await ctk.mint(donor2.getAddress(), mintAmount);
+  console.log("Minted CTK to donors");
+
+  // Approve the CrowdFunding contract to spend donors' tokens
+  await ctk.connect(donor1).approve(crowdfunding.getAddress(), mintAmount);
+  await ctk.connect(donor2).approve(crowdfunding.getAddress(), mintAmount);
+  console.log("Donors approved spending");
+
+  // Create a fundraising campaign
+  const goal = ethers.parseUnits("50", 18)
+  const duration = 60 * 10; // 10 minutes
+  const createTx = await crowdfunding
+    .connect(deployer)
+    .createFundRaising("Medical Aid", "Help for surgery", recipient.getAddress(), goal, duration);
+  await createTx.wait();
+  console.log("Created a crowdfunding campaign");
+
+  // Donate to the fundraising
+  const fundId = 1;
+  const donationAmount = ethers.parseUnits("25", 18)
+  await crowdfunding.connect(donor1).donate(fundId, donationAmount);
+  await crowdfunding.connect(donor2).donate(fundId, donationAmount);
+  console.log("Donors contributed to the campaign");
+
+  // End the fundraising campaign
+  await crowdfunding.connect(recipient).endFundRaising(fundId);
+  console.log("Fundraising campaign ended");
+
+  // Attempt refund if goal not met
+  try {
+    await crowdfunding.connect(donor1).refund(fundId);
+    console.log("Refund issued to donor1");
+  } catch (error) {
+    console.log("No refund required as goal was met");
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ Smart-contract/test/CrowdFunding.ts
@@ -0,0 +1,129 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre, { ethers } from "hardhat";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+
+describe("CrowdFunding Contract", () => {
+  const deployCrowdFundingContract = async () => {
+    const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
+
+    const [owner, donor1, donor2, stranger] = await ethers.getSigners();
+
+    // Deploy the ERC20 Token (CTK)
+    const Token = await ethers.getContractFactory("CTK");
+    const token = await Token.deploy();
+    await token.waitForDeployment();
+
+    // Transfer tokens to contributors
+    await token.transfer(donor1.address, 100); 
+    await token.transfer(donor2.address, 100);
+
+    // Deploy the CrowdFunding contract
+    const CrowdFunding = await ethers.getContractFactory("CrowdFunding");
+    const crowdFunding = await CrowdFunding.deploy(await token.getAddress());
+    await crowdFunding.waitForDeployment();
+
+    return { token, crowdFunding, owner, donor1, donor2, stranger, ADDRESS_ZERO };
+  };
+
+  describe("Deployment", () => {
+    it('should be deployed by owner', async() => {
+        let { crowdFunding, owner } = await loadFixture(deployCrowdFundingContract);
+
+        const runner = crowdFunding.runner as HardhatEthersSigner;
+
+        expect(runner.address).to.equal(owner.address);
+    });
+
+    it("Should deploy CrowdFunding with correct token address", async () => {
+      const { owner, crowdFunding } = await loadFixture(deployCrowdFundingContract);
+      expect(await crowdFunding.owner()).to.equal(await owner.getAddress());
+    });
+
+    it('should not be address zero', async() => {
+        let { crowdFunding, ADDRESS_ZERO } = await loadFixture(deployCrowdFundingContract);
+
+        expect(crowdFunding.target).to.not.be.equal(ADDRESS_ZERO);
+    }); 
+  });
+
+  describe("Creating a Fundraising Campaign", () => {
+    it("Should allow owner to create a fundraising campaign", async () => {
+      const { crowdFunding, owner } = await loadFixture(deployCrowdFundingContract);
+      const goal = ethers.parseEther("10"); // Goal: 10 tokens
+      const deadline = (await time.latest()) + 7 * 24 * 60 * 60; // 7 days from now
+      
+      await expect(crowdFunding.createFundRaising("Health Issue", "Help save Labake", owner, goal, deadline))
+        .to.emit(crowdFunding, "FundRaisingCreated")
+        .withArgs(0, goal, deadline, owner.address);
+
+      const campaign = await crowdFunding.fundRaisings(0);
+      expect(campaign.goal).to.equal(goal);
+      expect(campaign.deadline).to.equal(deadline);
+      expect(campaign.recipient).to.equal(owner.address);
+    });
+
+
+    it("Should fail if goal is zero", async () => {
+      const { crowdFunding, owner } = await loadFixture(deployCrowdFundingContract);
+      const goal = ethers.parseEther("0"); 
+      const deadline = (await time.latest()) + 7 * 24 * 60 * 60; // 7 days
+
+      await expect(crowdFunding.createFundRaising("Health Issue", "Help save Labake", owner, goal, deadline)).to.be.revertedWith("Goal must be greater than zero");
+    });
+  });
+
+  describe("Donating to Fundraising", () => {
+    it("Should allow users to donate", async () => {
+      const { crowdFunding, owner, token, donor1 } = await loadFixture(deployCrowdFundingContract);
+      const goal = ethers.parseEther("10");
+      const deadline = (await time.latest()) + 7 * 24 * 60 * 60;
+
+      await crowdFunding.createFundRaising("Health Issue", "Help save Labake", owner, goal, deadline);      
+      await token.transfer(donor1.address, ethers.parseEther("5"));
+      await token.connect(donor1).approve(await crowdFunding.getAddress(), ethers.parseEther("5"));
+
+      await expect(crowdFunding.connect(donor1).donate(0, ethers.parseEther("5")))
+        .to.emit(crowdFunding, "DonationReceived")
+    });
+
+    it("Should fail if donation amount is zero", async () => {
+      const { crowdFunding, owner, donor1 } = await loadFixture(deployCrowdFundingContract);
+      const goal = ethers.parseEther("10");
+      const deadline = (await time.latest()) + 7 * 24 * 60 * 60;
+
+      await crowdFunding.createFundRaising("Health Issue", "Help save Labake", owner, goal, deadline); 
+
+      await expect(crowdFunding.connect(donor1).donate(0, 0)).to.be.revertedWithCustomError(crowdFunding, "InvalidAmount");
+    });
+  });
+
+  describe("Ending a Fundraising Campaign", () => {
+    it("Should allow creator to end campaign after deadline", async () => {
+      const { crowdFunding, owner } = await loadFixture(deployCrowdFundingContract);
+      const goal = ethers.parseEther("10");
+      const deadline = (await time.latest()) + 5; 
+
+      await crowdFunding.createFundRaising("Health Issue", "Help save Labake", owner, goal, deadline); 
+      await time.increaseTo(deadline + 1); // Move past deadline
+
+      await expect(crowdFunding.endFundRaising(1))
+        .to.emit(crowdFunding, "FundRaisingEnded")
+        .withArgs(0, false);
+
+      const campaign = await crowdFunding.fundRaisings(1);
+      expect(campaign.hasEnded).to.be.true;
+    });
+
+    it("Should fail if campaign is still active", async () => {
+      const { crowdFunding, owner } = await loadFixture(deployCrowdFundingContract);
+      const goal = ethers.parseEther("10");
+      const deadline = (await time.latest()) + 7 * 24 * 60 * 60;
+
+
+      await crowdFunding.createFundRaising("Health Issue", "Help save Labake", owner, goal, deadline); 
+
+      await expect(crowdFunding.endFundRaising(1)).to.be.revertedWith("Fundraising is still active");
+    });
+  });
+});
